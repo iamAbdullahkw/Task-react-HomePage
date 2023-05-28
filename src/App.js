5e2efce1cda724c697f1661cf7fc21b07d245c11
Abdullah Fahad <iamabdullahkw@gmail.com>
2023-05-28 21:27:09 +0300

    two tasks done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BakeHouse from "./assets/download.jpeg";
+import products from "./products";
 
 function App() {
+  const myArray = products.map((product) => {
+    return (
+      <div>
+        <h2>{product.names}</h2>
+        <h2>{product.price}</h2>
+        <img src={product.Image} />
+      </div>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="center">
+        <h1>Welcome to your bakery</h1>
+        <p> the best bakery in the world </p>
+        <img src={BakeHouse} alt="bakery house" />
+      </div>
+      <div>
+        <h1></h1>
+      </div>
+      <div className="makeThisFlex">{myArray}</div>
+    </>
   );
 }
 
---
+++ src/products.js
@@ -0,0 +1,17 @@
+import cheescake from "./assets/download (2).jpeg";
+import cheescakeB from "./assets/download 2.jpeg";
+
+const products = [
+  {
+    names: "cheescake",
+    price: 12,
+    Image: cheescake,
+  },
+  {
+    names: "cheescake",
+    price: 12,
+    Image: cheescake,
+  },
+];
+
+export default products;
